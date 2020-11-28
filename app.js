42d48b3ee8cf6f3aa5f5df8bf18cfaaa9bb9a512
PavelSkorinov <pavlo.skorinov@gmail.com>
2020-11-28 17:02:19 +0200

    complete lesson 6 with bonuses

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
-const num = +prompt('How many levels high should be the tree?');
-let result = '';
-    for (let i = 0; i < num; i++) {
-        let star = '*'.repeat(2 * i + 1);
-        let spaceBefore = ' '.repeat(num - i - 1);
-        result += spaceBefore + star + `\n`;
+function cylinder(radius, height) {
+    let square = 2 * Math.PI * radius * height
+    let volume = square * height
+    let squareCorrect = square.toFixed(2)
+    let volumeCorrect = volume.toFixed(2)
+    alert("Площадь цилиндра - " + squareCorrect) 
+    alert("Объем цилиндра - " + volumeCorrect)
+};
+
+// cylinder(6, 6);
+
+
+function getResult(firstNum, secNum, symbol) {
+    if (symbol == "+") {
+
+        alert(firstNum + secNum);
+
+    } else if (symbol == "-") {
+
+        alert(firstNum - secNum);
+
+    } else if (symbol == "*") {
+
+        alert(firstNum * secNum);
+
+    } else if (symbol == "**") {
+
+        alert(firstNum ** secNum);
+
+    } else if (symbol == "/") {
+
+        alert(firstNum / secNum);
+
+    }
+};
+
+// getResult(2, 4, "**");
+
+
+function isCharPresent(str, symbol) {
+    if (str.search(symbol) != -1) {
+        alert(true);
+    } else {
+        alert(false);
+    }
+}
+
+// isCharPresent("Da", "9");
+
+
+function charIndexOf(str, symbol) {
+    if (str.search(symbol) != -1) {
+        alert(symbol);
+    } else {
+        alert(-1);
     };
-    alert(result);
+}
+
+// charIndexOf("Anton", "o");
