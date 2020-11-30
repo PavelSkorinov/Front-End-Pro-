fe9f93231899a0ee09d7d625d827f7a2fbc4c708
PavelSkorinov <pavlo.skorinov@gmail.com>
2020-11-30 13:48:17 +0200

    complete lesson 5

--- app.js
+++ app.js
@@ -1,65 +1,15 @@
-function calcCylinder(radius, height) {
-    var square = 2 * Math.PI * radius * height;
-    var volume = square * height;
-    var cylinder = {
-        square: square.toFixed(2),
-        volume: volume.toFixed(2)
-    }
-
-    return cylinder
-
+function padString(str, length, symbol, toStart = false) {
+    var resultString = str
+    if(str.length < length) {
+        var repeatedString = symbol.repeat(length - str.length);
+        if (toStart === true) {
+            resultString = repeatedString + resultString;
+        } else {
+            resultString = resultString + repeatedString;
+        }
+       
+    }   
+    return resultString; 
 };
 
-// console.log(calcCylinder(6, 6));
-
-
-
-function getResult(firstNum, secNum, symbol) {
-    if (symbol == "+") {
-
-        return firstNum + secNum;
-
-    } else if (symbol == "-") {
-
-        return firstNum - secNum;
-
-    } else if (symbol == "*") {
-
-        return firstNum * secNum;
-
-    } else if (symbol == "**") {
-
-        return firstNum ** secNum;
-
-    } else if (symbol == "/") {
-
-        return firstNum / secNum;
-
-    }
-};
-
-// console.log(getResult(2, 4, "**"));
-
-
-function isCharPresent(str, symbol) {
-    for ( i = 0; i < str.length; i++) {
-        if (str.startsWith(symbol, i) == true) {
-            return true
-        }
-    };   
-    return false 
-}
-
-// console.log(isCharPresent("Da", "u"));
-
-
-function charIndexOf(str, symbol) {
-    for ( i = 0; i < str.length; i++) {
-        if (str.startsWith(symbol, i) == true) {
-            return i
-        }
-    };   
-    return -1 
-}
-
-// console.log(charIndexOf("Bomba", "d"));
+console.log(padString('Toha', 8, '*', true));
