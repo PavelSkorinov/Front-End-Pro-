2fe00dc9db21eabeaffbb6a6750a9c73fc23f40e
PavelSkorinov <pavlo.skorinov@gmail.com>
2020-12-03 17:30:41 +0200

    Complete lesson 7

--- app.js
+++ app.js
@@ -1,15 +1,69 @@
-function padString(str, length, symbol, toStart = false) {
-    var resultString = str
-    if(str.length < length) {
-        var repeatedString = symbol.repeat(length - str.length);
-        if (toStart === true) {
-            resultString = repeatedString + resultString;
-        } else {
-            resultString = resultString + repeatedString;
-        }
-       
-    }   
-    return resultString; 
-};
+var arr = [1, 2, 3, 4];
+function map(array, mapFn) {
+    return array.reduce(function(resultArr, el) {           
+        resultArr.push(mapFn(el));
+        return resultArr;
+    }, [])
+}
+  
+//   var res = map(arr, function(el) {
+//     return el * 2
+//   })
+//   console.log(res);
 
-console.log(padString('Toha', 8, '*', true));
+
+var arr2 = [1, 2, 3, 4];
+function filter(array, filterFn) {
+    return array.reduce(function(resultArr, el) {
+        var canAdd = filterFn(el);
+        if (canAdd) {
+            resultArr.push(el);
+        }  
+        return resultArr;         
+    }, [])
+}
+
+//   var res = filter(arr, function(el) {
+//     return el % 2 === 0; 
+//   })
+//   console.log(res)
+
+
+
+const notification = [
+    {
+        date: '31/07/2019',
+        msg: 'Some message here'
+    },
+    {
+        date: '15/11/2017',
+        msg: 'And some message here'
+    },
+    {
+        date: '15/11/2017',
+        msg: 'And also some message here'
+    },
+    {
+        date: '31/07/2019',
+        msg: 'Last message here'
+    }
+]
+
+function groupByMsg(arr) {
+    return arr.reduce(function(result, el) {
+  var date = el.date;
+  if (!(date in result)) {
+    result[date] = [];
+  }
+  result[date].push(el);
+  return result;
+}, {});
+} 
+  
+var res = groupByMsg(notification);
+console.log(res);
+
+
+  
+
+  
