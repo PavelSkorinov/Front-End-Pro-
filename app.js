916c4f0b71af4aab33fad56cbe4046ebb003cfa9
PavelSkorinov <pavlo.skorinov@gmail.com>
2020-12-18 00:00:03 +0200

    fixed lesson 11

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let bind = function(fn, context) {
-    const rest = Array.prototype.slice.call(arguments, 2);
+    const arr = []
+    for (let i = 2; i < arguments.length; i++) {
+        arr.push(arguments[i])
+    }
     return function() {
-        const args = Array.prototype.slice.call(arguments);
-       return fn.apply(context, rest);
+        const arr2 = []
+        for (let i = 0; i < arguments.length; i++) {
+            arr2.push(arguments[i])
+        }
+       return fn.apply(context, arr.concat(arr2));
     };
 }
 
