237ec2eeda24f669149ee79b1569be9c7bb90915
PavelSkorinov <pavlo.skorinov@gmail.com>
2020-12-19 12:02:10 +0200

    complete lesson 13

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let bind = function(fn, context) {
-    const arr = []
-    for (let i = 2; i < arguments.length; i++) {
-        arr.push(arguments[i])
-    }
-    return function() {
-        const arr2 = []
-        for (let i = 0; i < arguments.length; i++) {
-            arr2.push(arguments[i])
-        }
-       return fn.apply(context, arr.concat(arr2));
+let sum = function(initial) {
+    let def = initial;
+
+    return function (count) {
+        def += count;
+        return def;
     };
-}
-
-
-const user = {
-    name: "Anton"
 };
 
-function userInfo(surname, age) {
-    console.log(`Name: ${this.name}, Surname: ${surname}, Age: ${age}`);
-}
+let result = sum(0);
 
-// userInfo.bind(user, 'Hanton', '22')()
+// console.log(result(3));
+// console.log(result(3));
+// console.log(result(8));
 
-bind(userInfo, user, 'Hanton', '22')();
+function createCounter(startValue, stepValue) {
+    let count = startValue;
+    const step = stepValue;
+    return function (reset = false) {
+        if (reset) {
+            count = startValue;
+        } else {
+            count += step;
+        };
+        return count;
+    };
+};
+
+const counter = createCounter(12, 20);
+
+console.log(counter());
+console.log(counter(true));
+console.log(counter());
+console.log(counter(true));
