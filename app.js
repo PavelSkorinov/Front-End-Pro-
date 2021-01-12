561dbe8b8fdc8fc782fe54be39c5e18c337db5ec
PavelSkorinov <pavlo.skorinov@gmail.com>
2021-01-12 20:22:18 +0200

    complete lesson 19

--- app.js
+++ app.js
@@ -1,111 +1,61 @@
-const TASKS_KEY = "tasks";
-class List {
-    tasks = []; 
 
-    constructor(name) {
-        this.name = name;
-    };
+function generateList(array) {
 
-    get tasks() {
-        const value = localStorage.getItem(TASKS_KEY);
-        if(value) {
-         return JSON.parse(value);
-        }
-        return this.tasks;
-    };
+    const $ul = document.createElement('ul');
 
-    createTask(title) {
-        let task = {
-            id: Date.now(),
-            title: title,
-            status: 'In Progress'
+    for (let i = 0; i < array.length; i++) { 
+
+        const $li = document.createElement('li');
+
+        if (Array.isArray(array[i])) {
+
+            $li.appendChild(generateList(array[i]));
+
+        } else {
+            $li.innerHTML = array[i];
         };
-        this.tasks.push(task);
-        this.toLocalStorage();
-        return task;
+
+        $ul.appendChild($li);
     };
 
-    deleteTask(index) {
-        let deletedTask = this.tasks[index];
-        this.tasks.splice(index, 1);
-        this.toLocalStorage();
-        return deletedTask;
-    };
+    return $ul;
 
-    changeTask(updTask, newTitle) {
-        let index = this.tasks.findIndex((el) => el.id === updTask.id);
-        console.log(updTask)
-        this.tasks.splice(index, 1, {
-            id: updTask.id,
-            title: newTitle,
-            status: updTask.status  
-        });
-        this.toLocalStorage();
-        return this.tasks[index];
-    };
+};
+const arr = [1, [1.1, 1.2, [1.21, [1.22, 1.222]], 1.3], 2, 3];
 
-    toLocalStorage() {     
-        localStorage.setItem(TASKS_KEY, JSON.stringify(this.tasks));
+document.body.appendChild(generateList(arr));
+
+const $table = document.createElement('table');
+$table.style.borderCollapse = 'collapse';
+$table.style.border = '1px solid black';
+const $tBody = document.createElement('tbody');
+const $tr = document.createElement('tr');
+$tr.style.height = '30px'
+const $td = document.createElement('td');
+$td.style.border = '1px solid black'
+$td.style.textAlign = 'center'
+$td.style.width = '30px'
+let i = 0;
+let count = 1;
+while (i < 10) {
+    const row = $tr.cloneNode();
+
+    j = 0;
+
+    while (j < 10) {
+        const column = $td.cloneNode()
+
+        column.innerHTML = count;
+
+        row.appendChild(column);
+        j++;
+        count++;
     };
+    $tBody.appendChild(row);
+    i++;
 };
 
-class ToDoList extends List {
+$table.appendChild($tBody);
+document.body.appendChild($table);
 
-    constructor(name) {
-        super(name)
-    };
 
-    setComplete(task) {
-        let index = this.tasks.findIndex((el) => el.id === task.id);
-        this.tasks.splice(index, 1, {
-            id: task.id,
-            title: task.title,
-            status: "Completed" 
-        });
-        this.toLocalStorage();
-        return this.tasks[index];
-    };
-
-    showAllTasks() {
-        const tasksInProgress = this.tasks.filter((task) => task.status === 'In Progress')
-        const tasksCompleted = this.tasks.length - tasksInProgress.length;
-        const taskStatus = {
-            completed: tasksCompleted,
-            uncompleted: tasksInProgress.length
-        };
-        return taskStatus;
-    };
-};
-
-class ContactList extends List {
-
-    constructor(name) {
-        super(name);
-    };
-
-    findByTitle(title) {
-        const filteredValue = this.tasks.filter((task) => task.title === title)
-        return filteredValue;
-    }
-};
-
-const contactList = new ContactList('First Contact List');
-const taskList = new ToDoList('First todo')
-taskList.createTask("Task #1");
-contactList.createTask("Anton Sukhaiev");
-contactList.createTask("Zdes mojet byt vasha reklama");
-contactList.createTask("Andrei Petrov");
-contactList.createTask("Anatoliy Klimchuk");
-taskList.createTask("Task #2");
-contactList.createTask("Zdes mojet byt vasha reklama");
-contactList.createTask("Vasya Pupkin");
-contactList.createTask("Petya Dyachenko");
-contactList.createTask("Zdes mojet byt vasha reklama");
-contactList.createTask("Oleg Kozlovskiy");
-taskList.deleteTask(1);
-taskList.createTask("Task #3");
-taskList.changeTask(taskList.tasks[0], "Task321312321");
-taskList.setComplete(taskList.tasks[0], "Task321312321");
-taskList.showAllTasks();
-console.log(taskList);
-console.log(contactList.findByTitle("Zdes mojet byt vasha reklama"));
